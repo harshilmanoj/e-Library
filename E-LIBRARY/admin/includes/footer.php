e55e11fb6abaecf0cc1bdb332ba1457c3ac4b318
Harshil Manoj <78437452+harshilmanoj@users.noreply.github.com>
2023-11-23 12:58:11 +0530

    Add files via upload

---
+++ E-LIBRARY/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; E-Library 2022 |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : Harshil 20BCT0125</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
